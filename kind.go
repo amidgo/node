4245349cea4bd22b8bcbb6e47f04b3eddf931c56
amidgo <mcdvaran@gmail.com>
2024-05-14 18:35:36 +0400

    add string to kind

--- kind.go
+++ kind.go
@@ -10,3 +10,22 @@ const (
 	Empty
 	Content
 )
+
+func (k Kind) String() string {
+	switch k {
+	case String:
+		return "String"
+	case Integer:
+		return "Integer"
+	case Float:
+		return "Float"
+	case Bool:
+		return "Bool"
+	case Empty:
+		return "Empty"
+	case Content:
+		return "Content"
+	default:
+		return "Unknown Kind"
+	}
+}
