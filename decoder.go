47664c93326f7e04d16be8a80dc18bbc08d2dea1
amidgo <mcdvaran@gmail.com>
2024-06-30 18:23:48 +0400

    add Must[Decode|Encode] funcs

--- decoder.go
+++ decoder.go
@@ -1,6 +1,9 @@
 package node
 
-import "io"
+import (
+	"io"
+	"log"
+)
 
 type Decoder interface {
 	Decode(data []byte) (Node, error)
@@ -8,4 +11,22 @@ type Decoder interface {
 
 type DecoderFrom interface {
 	DecodeFrom(src io.Reader) (Node, error)
+}
+
+func MustDecode(decoder Decoder, data []byte) Node {
+	nd, err := decoder.Decode(data)
+	if err != nil {
+		log.Panicf("decoder.Decode failed, %s", err)
+	}
+
+	return nd
+}
+
+func MustDecodeTo(decoder DecoderFrom, src io.Reader) Node {
+	nd, err := decoder.DecodeFrom(src)
+	if err != nil {
+		log.Panicf("decoder.DecodeFrom failed, %s", err)
+	}
+
+	return nd
 }
--- encoder.go
+++ encoder.go
@@ -2,6 +2,7 @@ package node
 
 import (
 	"io"
+	"log"
 )
 
 type Encoder interface {
@@ -10,4 +11,20 @@ type Encoder interface {
 
 type EncoderTo interface {
 	EncodeTo(w io.Writer, node Node) error
+}
+
+func MustEncode(encoder Encoder, nd Node) []byte {
+	data, err := encoder.Encode(nd)
+	if err != nil {
+		log.Panicf("encoder.Encode failed, %s", err)
+	}
+
+	return data
+}
+
+func MustEncodeTo(encoder EncoderTo, dst io.Writer, nd Node) {
+	err := encoder.EncodeTo(dst, nd)
+	if err != nil {
+		log.Panicf("encoder.EncodeTo failed, %s", err)
+	}
 }
